e8bc277b2257fd63abc082ce0707587ca6149306
Taha Yıldırım <thyldrm23@gmail.com>
2024-11-19 14:36:41 +0300

    fix: update project check error handler

--- utils.js
+++ utils.js
@@ -37,22 +37,22 @@ const failedArgs = (failedArgsParsed) => {
         weakness_is,
         automerge,
         condition,
-      },
+      }
     ) => {
       return {
         ...acc,
-        max_number_of_critical: max_number_of_critical ||
-          acc.max_number_of_critical,
+        max_number_of_critical:
+          max_number_of_critical || acc.max_number_of_critical,
         max_number_of_high: max_number_of_high || acc.max_number_of_high,
         weakness_is: weakness_is || acc.weakness_is,
         automerge: automerge || acc.automerge,
         condition: condition || acc.condition,
       };
     },
-    {},
+    {}
   );
   return output;
-}
+};
 
 const login = async (CT_BASE_URL, CT_USERNAME, CT_PASSWORD) => {
   let responseToken;
@@ -62,29 +62,34 @@ const login = async (CT_BASE_URL, CT_USERNAME, CT_PASSWORD) => {
       client_secret: CT_PASSWORD,
     });
   } catch (error) {
-    if(error?.response?.data?.message) throw new Error(error.response.data.message);
-    else throw new Error(JSON.stringify(error))
+    if (error?.response?.data?.message)
+      throw new Error(error.response.data.message);
+    else throw new Error(JSON.stringify(error));
   }
-  console.log("[CodeThreat]: Login successful")
+  console.log("[CodeThreat]: Login successful");
   return responseToken.data.access_token;
 };
 
 const check = async (CT_BASE_URL, projectName, authToken, CT_ORGANIZATION) => {
   let checkProject;
   try {
-    checkProject = await axios.get(`${CT_BASE_URL}/api/project?key=${projectName}`, {
-      headers: {
-        Authorization: authToken,
-        "x-ct-organization": CT_ORGANIZATION,
-      },
-    });
+    checkProject = await axios.get(
+      `${CT_BASE_URL}/api/project?key=${projectName}`,
+      {
+        headers: {
+          Authorization: authToken,
+          "x-ct-organization": CT_ORGANIZATION,
+        },
+      }
+    );
   } catch (error) {
-    if (error.response.data.code === 400 || error.response.data.code === 404) {
+    if (error.response.data.code === 404) {
       return {
         type: null,
       };
-    }
-    else throw new Error(JSON.stringify(error))
+    } else if (error.response && error.response.data) {
+      throw new Error(JSON.stringify(error.response.data));
+    } else throw new Error(JSON.stringify(error));
   }
   if (checkProject.data.type !== "gitlab") {
     throw new Error(
@@ -127,10 +132,11 @@ const create = async (
       }
     );
   } catch (error) {
-    if(error?.response?.data?.message) throw new Error(error.response.data.message);
-    else throw new Error(JSON.stringify(error))
+    if (error?.response?.data?.message)
+      throw new Error(error.response.data.message);
+    else throw new Error(JSON.stringify(error));
   }
-  console.log("[CodeThreat]: Project Created.")
+  console.log("[CodeThreat]: Project Created.");
   return createProject;
 };
 
@@ -173,11 +179,26 @@ const start = async (
         },
       }
     );
+    if (scanStart.status === 200 && scanStart.data.scan_id) return scanStart;
+    else {
+      console.log(
+        `Failed to start scan. Status: ${JSON.stringify(
+          scanStart.status
+        )} Error: ${JSON.stringify(
+          scanStart.data || { error: "Unexpected Error: Scan Start" }
+        )}`
+      );
+      throw new Error(
+        JSON.stringify(
+          scanStart.data || { error: "Unexpected Error: Scan Start" }
+        )
+      );
+    }
   } catch (error) {
-    if(error?.response?.data?.message) throw new Error(error.response.data.message);
-    else throw new Error(JSON.stringify(error))
+    if (error.response && error.response.data)
+      throw new Error(JSON.stringify(error.response.data));
+    else throw new Error(error);
   }
-  return scanStart;
 };
 
 const status = async (CT_BASE_URL, sid, authToken, CT_ORGANIZATION) => {
@@ -187,12 +208,13 @@ const status = async (CT_BASE_URL, sid, authToken, CT_ORGANIZATION) => {
       headers: {
         Authorization: authToken,
         "x-ct-organization": CT_ORGANIZATION,
-        "plugin": true,
+        plugin: true,
       },
     });
   } catch (error) {
-    if(error?.response?.data?.message) throw new Error(error.response.data.message);
-    else throw new Error(JSON.stringify(error))
+    if (error?.response?.data?.message)
+      throw new Error(error.response.data.message);
+    else throw new Error(JSON.stringify(error));
   }
   severityLevels.forEach((level) => {
     severities[level] = scanProcess.data.sast_severities?.[level] || 0;
@@ -208,45 +230,65 @@ const status = async (CT_BASE_URL, sid, authToken, CT_ORGANIZATION) => {
   };
 };
 
-const result = async (CT_BASE_URL, sid, authToken, CT_ORGANIZATION, branch, project_name) => {
+const result = async (
+  CT_BASE_URL,
+  sid,
+  authToken,
+  CT_ORGANIZATION,
+  branch,
+  project_name
+) => {
   let resultScan;
   try {
-    resultScan = await axios.get(`${CT_BASE_URL}/api/plugins/helper?sid=${sid}&branch=${branch}&project_name=${project_name}`, {
-      headers: {
-        Authorization: authToken,
-        "x-ct-organization": CT_ORGANIZATION,
-        "x-ct-from": 'gitlab'
-      },
-    });
+    resultScan = await axios.get(
+      `${CT_BASE_URL}/api/plugins/helper?sid=${sid}&branch=${branch}&project_name=${project_name}`,
+      {
+        headers: {
+          Authorization: authToken,
+          "x-ct-organization": CT_ORGANIZATION,
+          "x-ct-from": "gitlab",
+        },
+      }
+    );
   } catch (error) {
-    if(error.response.status === 404) return { type: null }
-    if(error?.response?.data?.message) throw new Error(error.response.data.message);
-    else throw new Error(JSON.stringify(error))
+    if (error.response.status === 404) return { type: null };
+    if (error?.response?.data?.message)
+      throw new Error(error.response.data.message);
+    else throw new Error(JSON.stringify(error));
   }
-  return {report: resultScan.data.report, scaSeverityCounts: resultScan.data.scaSeverityCounts};
-}
+  return {
+    report: resultScan.data.report,
+    scaSeverityCounts: resultScan.data.scaSeverityCounts,
+  };
+};
 
 const saveSarif = async (ctServer, sid, authToken, orgname) => {
   try {
-    const response = await axios.get(`${ctServer}/api/report/scan/create?sid=${sid}&reportType=sarif`, {
-      headers: {
-        Authorization: authToken,
-        "x-ct-organization": orgname,
-        "x-ct-from": 'gitlab'
-      },
-    });
+    const response = await axios.get(
+      `${ctServer}/api/report/scan/create?sid=${sid}&reportType=sarif`,
+      {
+        headers: {
+          Authorization: authToken,
+          "x-ct-organization": orgname,
+          "x-ct-from": "gitlab",
+        },
+      }
+    );
 
     const sarifData = JSON.stringify(response.data.parsedResult);
 
-    const artifactPath = 'codethreat.sarif';
+    const artifactPath = "codethreat.sarif";
     await fs.writeFile(artifactPath, sarifData);
 
     console.log(`[CodeThreat]: Sarif report saved to ${artifactPath}`);
   } catch (error) {
-    throw new Error(`Failed to save SARIF report: ${error.response?.data?.message || error.message}`);
+    throw new Error(
+      `Failed to save SARIF report: ${
+        error.response?.data?.message || error.message
+      }`
+    );
   }
 };
-
 
 module.exports = {
   findWeaknessTitles,
@@ -257,5 +299,5 @@ module.exports = {
   start,
   status,
   result,
-  saveSarif
+  saveSarif,
 };
